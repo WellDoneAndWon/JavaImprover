dcbafd59a2ff01e38280bd2c20e10c7040f39624
WellDoneAndWon <ildanaxy@gmail.com>
2023-02-09 00:30:10 +0500

    WellDone

--- src/generics/game/Team.java
+++ src/generics/game/Team.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Team {
+public class Team<T extends Participant> {
     private String name;
-    private List<Participant> participants = new ArrayList<>();
+    private List<T> participants = new ArrayList<>();
 
     public Team(String name) {
         this.name = name;
     }
-    public void addNewParticipant(Participant participant) {
+    public void addNewParticipant(T participant) {
         participants.add(participant);
         System.out.println("Team " + " have new player, his/her name is " + participant.getName());
     }
-    public void playWith(Team team){
+    public void playWith(Team<T> team){
         String winnerName;
         Random random = new Random();
         int i = random.nextInt(2);
--- src/generics/game/Test.java
+++ src/generics/game/Test.java
@@ -10,5 +10,25 @@ public class Test {
 
         Employee employee1 = new Employee("Simon",33);
         Employee employee2 = new Employee("Lara",31);
+
+        Team<Schollar> schoolarTeam = new Team("LookOutWorld");
+        Team<Student> studentTeam = new Team("Smarties");
+        Team<Employee> employeeTeam = new Team("Enjoyers");
+
+        schoolarTeam.addNewParticipant(schollar1);
+        schoolarTeam.addNewParticipant(schollar2);
+
+        studentTeam.addNewParticipant(student1);
+        studentTeam.addNewParticipant(student2);
+
+        employeeTeam.addNewParticipant(employee1);
+        employeeTeam.addNewParticipant(employee2);
+
+        Team<Schollar> schoolarTeam2 = new Team("SoBeIt");
+        Schollar schollar3 = new Schollar("Cozy",12);
+        Schollar schollar4 = new Schollar("Rose",14);
+        schoolarTeam2.addNewParticipant(schollar3);
+        schoolarTeam2.addNewParticipant(schollar4);
+        schoolarTeam.playWith(schoolarTeam2);
     }
 }
