25fd58302114da48aac02dcc431d42065e20b76f
WellDoneAndWon <ildanaxy@gmail.com>
2023-02-08 23:59:41 +0500

    WellDone

---
+++ src/generics/game/Test.java
@@ -0,0 +1,14 @@
+package generics.game;
+
+public class Test {
+    public static void main(String[] args) {
+        Schollar schollar1 = new Schollar("Ivan",11);
+        Schollar schollar2 = new Schollar("Tom",13);
+
+        Student student1 = new Student("John",18);
+        Student student2 = new Student("Sarah",19);
+
+        Employee employee1 = new Employee("Simon",33);
+        Employee employee2 = new Employee("Lara",31);
+    }
+}
